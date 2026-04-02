ebafa68d1dee5799635c418c2abe327fd6f0d8a2
Nhut Nguyen <ngnhut07051998@gmail.com>
2026-04-02 15:55:40 +0700

    chore: test lint-staged

--- apps/la-bidder-v0/src/components/bidder/something-add/something-add.tsx
+++ apps/la-bidder-v0/src/components/bidder/something-add/something-add.tsx
@@ -1,7 +1,7 @@
-import Button from "@/components/ui/button/button";
-import Card from "@/components/ui/card/card";
-import { Outfit } from "next/font/google";
-import Image from "next/image";
+import Button from '@/components/ui/button/button';
+import Card from '@/components/ui/card/card';
+import { Outfit } from 'next/font/google';
+import Image from 'next/image';
 
 const outfit = Outfit();
 
@@ -10,19 +10,28 @@ export default function SomethingAdd(): React.ReactNode {
     <Card className={`${outfit.className} h-16`}>
       <div className="flex justify-between w-full">
         <div className="flex gap-3">
-          <Image src='ethereum.svg' alt='ethereum-logo' width={32} height={32}></Image>
+          <Image
+            src="ethereum.svg"
+            alt="ethereum-logo"
+            width={32}
+            height={32}
+          ></Image>
           <span className="text-[20px] font-bold">4,668 ETH</span>
         </div>
 
         <Button>
           <span className="inline-flex gap-[6px]">
             Add
-
             {/* TODO: icon lib? */}
-            <Image src='chevron-right.svg' alt="chevron-right" width={16} height={16}></Image>
+            <Image
+              src="chevron-right.svg"
+              alt="chevron-right"
+              width={16}
+              height={16}
+            ></Image>
           </span>
         </Button>
       </div>
     </Card>
-  )
+  );
 }
